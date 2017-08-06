3f0550344f11f3e5e68401b5545dc0c3e91fbbab
Patrick Ferreira <patrickctrf@hotmail.com.br>
2017-08-06 03:43:08 -0300

    Primeira versão do código completa.

--- src/main.c
+++ src/main.c
@@ -14,7 +14,7 @@ int main(){
   int QtdPalavras = 0, resultado = 0;
   char *str = NULL;//O ponteiro para array de caracteres que guardará nossa string.
 
-  scanf("%[^\n]", str);//Recebemos a string de entrada até ser inserido o caractere de uebra de linha.
+  scanf("%[^\n]", str);//Recebemos a string de entrada até ser inserido o caractere de quebra de linha.
   
   if(str != NULL) resultado = trataString(str);//Somente chama a função se a string str não estiver VAZIA (quando o usuário não insere nenhuma palavra e clica enter).
   
@@ -30,22 +30,41 @@ int main(){
 int trataString(char *str){
   int i = 0;
   int QtdPalavras = 0;
-  char ultimo = -1, penultimo = -1;
+  char ultimo = -1, penultimo = -1;//Guardam os valores dos ultimos caracteres analisados para serem utilizados em condiçes de contorno. Iniciam com valor -1 para não serem confundidos com nenhum caractere antes de serem realmente preenchidos pela entrada de texto.
   
   if(str == NULL) return -1;
   
   for(i = 0; str[i] != '\0'; i++){
     
+    if(conjunto(str[i]) == 0 && conjunto[ultimo] != 0){//Se estamos sobre um separador e o caractere anterior não era separador, conte mais uma palavra.
+      
+      QtdPalavras++;
+      
+    }
+      
+    if(conjunto(str[i]) == 1 && (ultimo == '.' || ultimo == ',') && conjunto[penultimo] == 1){//Esta condição indica que passamos por um número de ponto flutuante.
+      
+      QtdPalavras--;//Os algarismos de um ponto flutuante serão contados como duas palavras quando deveriam ser apenas uma. Nesta linha, descontamos este erro.
+      
+    }
     
+    //Atualizando os caracteres.
+    penultimo = ultimo;
+    ultimo = str[i];
     
   }
+  
+  //Se a frase não terminou com um ponto, possivelmente, nao teremos contado a última palavra. Aqui verificamos e corrigimos isso.
+  if(conjunto(ultimo) == 2 || conjunto(ultimo) == 1) QtdPalavras++;
+  
+  return QtdPalavras;
   
 }
 
 /**
  * Esta função verifica a qual conjunto de caracteres o dado argumento pertence.
  * Retorna 2, se for uma letra, 1, se for um número, e 0 (zero) se for qual-
- * quer outro caractere.
+ * quer outro caractere (separador).
  */
 int conjunto(char c){
   
